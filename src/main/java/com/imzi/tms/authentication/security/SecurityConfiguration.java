bb02b5678d9aa676bf2702848abefeaeff09989f
imzi <amimthath@gmail.com>
2023-10-24 23:39:48 +0530

    Started OAuth

---
+++ src/main/java/com/imzi/tms/authentication/security/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.imzi.tms.authentication.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
